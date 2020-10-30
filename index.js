a02ccf43393af0d7875eaaae5ed0d7ce6bf52016
Luiz Maffeis <lmaffeis@hotmail.com>
2020-10-30 15:53:19 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const port = 8080;
+
+//Express
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+//bodyparser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Rotas
+app.get("/", (req, res) => {
+    res.render("index", {    });
+});
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar")
+});
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulario recebido " + titulo +" " + descricao);
+});
+
+app.listen(port, () => { console.log("servidor rodando, porta: " + port) });
